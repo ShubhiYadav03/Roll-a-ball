121552defeae8fb175e48acd14dd4fd0268d4312
Shubhi Yadav <shubhi.yadav.1234@gmail.com>
2021-11-17 22:16:05 +0530

    Game added

---
+++ Rollaball/Assets/Scripts/PlayButton.cs
@@ -0,0 +1,22 @@
+﻿
+using UnityEngine;
+using TMPro;
+
+
+public class PlayButton : MonoBehaviour
+{
+    public TextMeshProUGUI username;
+    public TMP_InputField input;
+    public TextMeshProUGUI winText;
+    public GameObject canvas;
+    public GameObject player;
+
+    // Start is called before the first frame update
+    public void getValue()
+    {
+        string user = username.text; //here the value is "a"
+        winText.text = $"Congrats {user}!\n You Win!!";
+        player.SetActive(true);
+        canvas.SetActive(false);
+    }
+}
---
+++ Rollaball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class PlayerController : MonoBehaviour
+{
+    public AudioSource source;
+    public float speed = 0;
+    public TextMeshProUGUI countText;
+    public GameObject WinTextObject;
+    private Rigidbody rb;
+    private int count;
+    private float MovementX;
+    private float MovementY;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        source= GetComponent<AudioSource>();
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        setCountText();
+        WinTextObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+        MovementX = movementVector.x;
+        MovementY = movementVector.y;
+    }
+
+    void setCountText()
+    {
+        countText.text = "Count : " + count.ToString();
+        if (count >= 12)
+        {
+            WinTextObject.SetActive(true);
+          
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(MovementX, 0.0f, MovementY);
+        rb.AddForce(movement*speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            setCountText();
+            source.Play();
+        }
+        
+    }
+}
